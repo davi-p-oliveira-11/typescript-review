4ee01196d3d132b8afe6921c4e78fa8ccdc7a207
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-07-14 12:12:11 -0300

    first commit

---
+++ annotations.ts
@@ -0,0 +1,13 @@
+// Strings
+let myname: string = 'John Wick';
+myname = 'ballerina'
+console.log(myname);
+
+// Numbers
+let favNumber: number = 11;
+console.log(favNumber);
+
+// Boolean
+let tsHard: boolean = false;
+tsHard = true;
+console.log(tsHard);
