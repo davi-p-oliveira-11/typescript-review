34a2a835be0aa276d26f2ac425dcd5ee7fbf16bb
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-07-14 12:50:03 -0300

    updated function-annotations

---
+++ function-annotations.ts
@@ -0,0 +1,12 @@
+// Regular Function
+function addOne(num: number) {
+      return num + 1;
+}
+
+const res = addOne(3);
+console.log(res);
+
+// Arrow Function
+const double = (x: number, y: number) => x * y;
+const res2 = double(2, 10);
+console.log(res);
